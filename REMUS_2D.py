f78879f8d6beb7f1f75ff049d1b723ddb0a174a3
Bobli1987 <lither0627@gmail.com>
2015-08-02 23:13:00 -0400

    A major modification
    Drawing method is removed from the class.

--- REMUS_2D.py
+++ REMUS_2D.py
@@ -10,7 +10,6 @@ No control system is involved.
 import numpy as np
 import scipy.integrate as integrate
 import matplotlib.pyplot as plt
-import matplotlib.animation as animation
 import matplotlib.transforms as transforms
 import shelve
 
@@ -36,8 +35,7 @@ class REMUS:
     # define the parameter of the ocean current
     ocean_current = np.asarray([0, 0., 0], dtype='float')  # the velocity in the earth-fixed frame
 
-    def __init__(self, init_velocity=(1.0, 0, 0), init_position=(1, 0, 0), step_size = 0.05, step_number = 6000,
-                 animation_on = False):
+    def __init__(self, init_velocity=(1.0, 0, 0), init_position=(1, 0, 0), step_size = 0.05, step_number = 6000):
 
         # set the initial states of the vehicle
         self.init_velocity = np.asarray(init_velocity, dtype='float')
@@ -61,34 +59,6 @@ class REMUS:
         self.velocity_history = np.vstack((np.asarray([self.init_velocity]), np.zeros((self.step_number, 3))))
         self.relative_velocity_history = np.vstack((np.asarray([self.relative_velocity]),
                                                     np.zeros((self.step_number, 3))))
-
-        # define the figure of the animation
-        self.animation_on = animation_on
-        if self.animation_on:
-            self.fig = plt.figure(figsize=(12, 8), dpi=100)
-            self.ax = self.fig.add_subplot(111, aspect='equal', autoscale_on=False, xlim=(-20, 20), ylim=(-20, 20))
-            self.center, = self.ax.plot([], [], 'rx', markersize=6)
-            self.trajectory, = self.ax.plot([], [], 'b-', lw=1)
-            self.time_text = self.ax.text(0.02, 0.95, '', transform=self.ax.transAxes)
-
-            if np.linalg.norm(REMUS.ocean_current):
-                self.ocean_text = self.ax.text(0.80, 0.1, 'Ocean Current', transform=self.ax.transAxes, color='b',
-                                               bbox={'facecolor': 'red', 'alpha': 0.5, 'pad': 10})
-                self.ocean_arrow = plt.Arrow(0.90, 0.05, 0.2*REMUS.ocean_current[1], 0.2*REMUS.ocean_current[0],
-                                             facecolor='b', edgecolor='b', width=0.05, transform=self.ax.transAxes)
-            else:
-                self.ocean_text = self.ax.text(0.75, 0.1, 'No Ocean Current', transform=self.ax.transAxes, color='b',
-                                               bbox={'facecolor': 'red', 'alpha': 0.5, 'pad': 10})
-
-            self.ax.grid()
-            self.ax.set_xlabel(r"Longitude (m)")
-            self.ax.set_ylabel(r"Latitude (m)")
-            self.ax.set_title(r"The trajectory of the vehicle")
-
-            self.t_start = self.ax.transData
-            self.coords = self.t_start.transform([self.position[1], self.position[0]])
-            self.arrow = plt.Arrow(self.position[1], self.position[0], 0, 2,
-                                   facecolor='r', edgecolor='b', width=1.5, animated=True)
 
     @staticmethod
     def transformation_matrix(position):
@@ -166,104 +136,54 @@ class REMUS:
         self.velocity_history[self.step_counter] = self.velocity
         self.relative_velocity_history[self.step_counter] = self.relative_velocity
 
-    def animation_init(self):
-        """initialize animation"""
-        self.center.set_data([], [])
-        self.trajectory.set_data([], [])
-        self.time_text.set_text('')
-        self.ax.add_patch(self.arrow)
-        if np.linalg.norm(REMUS.ocean_current):
-            self.ax.add_patch(self.ocean_arrow)
-        return self.center, self.trajectory, self.time_text, self.arrow
-
-    def animate(self, frame_number):
-        """perform animation step"""
-        self.step()
-
-        # update the center and trajectory of the vehicle
-        self.center.set_data(self.position[1], self.position[0])
-
-        self.trajectory.set_data(self.position_history[:self.step_counter, 1],
-                                 self.position_history[:self.step_counter, 0])
-
-        # update the time displayed
-        self.time_text.set_text('time = %.3f sec' % self.time_elapsed)
-
-        # update the arrow which represents the vehicle
-        angle = -self.position[2] * 180/np.pi
-
-        tr = transforms.Affine2D().rotate_deg_around(self.coords[0], self.coords[1], angle)
-
-        trans = self.t_start.transform([self.position[1], self.position[0]]) - self.coords
-        tr = tr.translate(trans[0], trans[1])
-
-        t_end = self.t_start + tr
-
-        self.arrow.set_transform(t_end)
-
-        return self.center, self.trajectory, self.time_text, self.arrow
-
 # -------------------------------------------------------------------------------
 # The following codes are used to test the class defined above
 if __name__ == "__main__":
 
-#     # define an instance of REMUS
-#     vehicle = REMUS((1.0, 0.05, 0))
-#
-# # -------------------------------------------------------------------------------
-# # simulate the motion of the vehicle
-#     for step in xrange(0, vehicle.step_number):
-#         vehicle.step()
-#
-# # -------------------------------------------------------------------------------
-# # visualize the velocity history
-#     plt.figure(figsize=[10, 6])
-#     plt.plot(vehicle.time_vec, vehicle.velocity_history[:, 0],
-#              color="blue", linewidth=2.5, linestyle="-", label=r"$u$")
-#     plt.plot(vehicle.time_vec, vehicle.velocity_history[:, 1],
-#              color="green", linewidth=2.5, linestyle="-", label=r"$v$")
-#     plt.plot(vehicle.time_vec, vehicle.velocity_history[:, 2],
-#              color="red", linewidth=2.5, linestyle="-", label=r"$r$")
-#     plt.xlabel(r"time (sec)")
-#     plt.ylabel(r"velocity")
-#     plt.legend(loc='lower right')
-#     plt.grid(True)
-#
-# # visualize the heading angle history
-#     plt.figure(figsize=[10, 6])
-#     plt.plot(vehicle.time_vec, np.fmod(vehicle.position_history[:, 2] * 180/np.pi, np.asarray([360])),
-#              color="blue", linewidth=2.5, linestyle="-")
-#     plt.xlabel(r"time (sec)")
-#     plt.ylabel(r"heading angle (deg)")
-#     plt.title(r"The heading angle of the vehicle")
-#     plt.grid(True)
-#
-# # visualize the trajectory
-#     plt.figure(figsize=[10, 10])
-#     plt.plot(vehicle.position_history[:, 1], vehicle.position_history[:, 0], color="blue", linewidth=2.5, linestyle="-")
-#     plt.xlabel(r"Longitude (m)")
-#     plt.ylabel(r"Latitude (m)")
-#     plt.title(r"The trajectory of the vehicle on the horizontal plane")
-#     plt.grid(True)
-#     plt.show()
-#
-# # -------------------------------------------------------------------------------
-# # save the instance into a file
-#     dbase = shelve.open('vehicle_14.db')
-#     try:
-#         dbase['vehicle'] = vehicle
-#     finally:
-#         dbase.close()
+    # define an instance of REMUS
+    vehicle = REMUS((1.0, 0.05, 0))
 
 # -------------------------------------------------------------------------------
-    # define an instance of REMUS
-    vehicle = REMUS((1.0, 0.1, 0.2), animation_on=True)
-    # display the animation of the vehicle's trajectory
-    if vehicle.animation_on:
-        interval = 900 * vehicle.step_size
+# simulate the motion of the vehicle
+    for step in xrange(0, vehicle.step_number):
+        vehicle.step()
 
-        ani = animation.FuncAnimation(vehicle.fig, vehicle.animate, interval=interval, blit=True,
-                                      init_func=vehicle.animation_init)
-        plt.show()
-    else:
-        print("Error: Animation not available, please turn on the animation when initializing the instance!")
+# -------------------------------------------------------------------------------
+# visualize the velocity history
+    plt.figure(figsize=[10, 6])
+    plt.plot(vehicle.time_vec, vehicle.velocity_history[:, 0],
+             color="blue", linewidth=2.5, linestyle="-", label=r"$u$")
+    plt.plot(vehicle.time_vec, vehicle.velocity_history[:, 1],
+             color="green", linewidth=2.5, linestyle="-", label=r"$v$")
+    plt.plot(vehicle.time_vec, vehicle.velocity_history[:, 2],
+             color="red", linewidth=2.5, linestyle="-", label=r"$r$")
+    plt.xlabel(r"time (sec)")
+    plt.ylabel(r"velocity")
+    plt.legend(loc='lower right')
+    plt.grid(True)
+
+# visualize the heading angle history
+    plt.figure(figsize=[10, 6])
+    plt.plot(vehicle.time_vec, np.fmod(vehicle.position_history[:, 2] * 180/np.pi, np.asarray([360])),
+             color="blue", linewidth=2.5, linestyle="-")
+    plt.xlabel(r"time (sec)")
+    plt.ylabel(r"heading angle (deg)")
+    plt.title(r"The heading angle of the vehicle")
+    plt.grid(True)
+
+# visualize the trajectory
+    plt.figure(figsize=[10, 10])
+    plt.plot(vehicle.position_history[:, 1], vehicle.position_history[:, 0], color="blue", linewidth=2.5, linestyle="-")
+    plt.xlabel(r"Longitude (m)")
+    plt.ylabel(r"Latitude (m)")
+    plt.title(r"The trajectory of the vehicle on the horizontal plane")
+    plt.grid(True)
+    plt.show()
+
+# -------------------------------------------------------------------------------
+# save the instance into a file
+    dbase = shelve.open('vehicle_2D.db')
+    try:
+        dbase['vehicle'] = vehicle
+    finally:
+        dbase.close()
